71d295d698075f80fe6041c6250b69bb2327be79
Randy Pope <rpope@Randys-Work-MacBook.local>
2021-12-13 12:01:23 -0500

    added error handling to dbscript

--- wp1-web/db-dump-tool.py
+++ wp1-web/db-dump-tool.py
@@ -3,21 +3,30 @@
 import mysql.connector
 from tabulate import tabulate
 
-conn = mysql.connector.connect(user='wp_user', password='wordpress', host='wp-db',database='wordpress')
+try:
+    conn = mysql.connector.connect(connection_timeout=5, user='wp_user', password='wordpress', host='wp-db',database='wordpress')
 
-# if conn:
-#    print ("Connected Successfully")
-# else:
-#    print ("Connection Not Established")
+except mysql.connector.Error as err:
+    print("Content-type:text/html\n\n")
+    print("<head><title>Employee Database Dump Tool</title></head>\n")
+    print("<body>\n")
+    print("<h1>Employee Database Dump Tool</h1>\n")
+    print("<h2>Database Connection is broken</h2>")
+    print(err)
+else:
+    select_user = """SELECT * FROM wp_users"""
+    cursor = conn.cursor()
+    cursor.execute(select_user)
+    result = cursor.fetchall()
 
-select_user = """SELECT * FROM wp_users"""
-cursor = conn.cursor()
-cursor.execute(select_user)
-result = cursor.fetchall()
+    print("Content-type:text/html\n\n")
+    print("<head><title>Employee Database Dump Tool</title></head>\n")
+    print("<body>\n")
+    print("<h1>Employee Database Dump Tool</h1>\n")
+    print(tabulate(result, tablefmt="html"))
+    conn.close()
 
-print("Content-type:text/html\n\n")
-print("<head><title>Employee Database Dump Tool</title></head>\n")
-print("<body>\n")
-print("<h1>Employee Database Dump Tool</h1>\n")
-print(tabulate(result, tablefmt="html"))
+
+
+
 
