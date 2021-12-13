94abe8c5c6c8a7486c7a34353a66324a5aabd713
Randy Pope <rpope@Randys-Work-MacBook.local>
2021-12-13 10:11:35 -0500

    fixing wp1-web dockerfile

---
+++ wp1-web/small-app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import mysql.connector
+from tabulate import tabulate
+
+conn = mysql.connector.connect(user='wp_user', password='wordpress', host='wp-db',database='wordpress')
+
+# if conn:
+#    print ("Connected Successfully")
+# else:
+#    print ("Connection Not Established")
+
+select_user = """SELECT * FROM wp_users"""
+cursor = conn.cursor()
+cursor.execute(select_user)
+result = cursor.fetchall()
+
+print("Content-type:text/html\n\n")
+print("<head><title>Employee Database</title></head>\n")
+print("<body>\n")
+print("<h1>Employee Database</h1>\n")
+print(tabulate(result, tablefmt="html"))
+
