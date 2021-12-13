ff8cf1250f3ff025d8ca6b0bf5b43c7bdddeb947
Randy Pope <rpope@Randys-Work-MacBook.local>
2021-12-13 12:09:20 -0500

    added progress indicator to db query

--- wp1-web/db-dump-tool.py
+++ wp1-web/db-dump-tool.py
@@ -4,6 +4,12 @@ import mysql.connector
 from tabulate import tabulate
 
 try:
+    print("Content-type:text/html\n\n")
+    print("<head><title>Employee Database Dump Tool</title></head>\n")
+    print("<body>\n")
+    print("<h1>Employee Database Dump Tool</h1>\n")
+    print("<h2>Connecting to Database</h2>")
+    
     conn = mysql.connector.connect(connection_timeout=5, user='wp_user', password='wordpress', host='wp-db',database='wordpress')
 
 except mysql.connector.Error as err:
