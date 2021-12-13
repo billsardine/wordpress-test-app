40ab132f8185748288f8a7c77bae19f5170f25ba
Randy Pope <rpope@Randys-Work-MacBook.local>
2021-12-13 12:38:18 -0500

    fixed some of the html in html display

--- wp1-web/db-dump-tool.py
+++ wp1-web/db-dump-tool.py
@@ -8,28 +8,32 @@ try:
     print("<head><title>Employee Database Dump Tool</title></head>\n")
     print("<body>\n")
     print("<h1>Employee Database Dump Tool</h1>\n")
-    print("<h2>Connecting to Database</h2>")
-    
+
     conn = mysql.connector.connect(connection_timeout=5, user='wp_user', password='wordpress', host='wp-db',database='wordpress')
 
 except mysql.connector.Error as err:
-    print("Content-type:text/html\n\n")
-    print("<head><title>Employee Database Dump Tool</title></head>\n")
-    print("<body>\n")
-    print("<h1>Employee Database Dump Tool</h1>\n")
+#    print("Content-type:text/html\n\n")
+#    print("<head><title>Employee Database Dump Tool</title></head>\n")
+#    print("<body>\n")
+#    print("<h1>Employee Database Dump Tool</h1>\n")
     print("<h2>Database Connection is broken</h2>")
     print(err)
+    print("</body>")
+    print("</html>")
+
 else:
     select_user = """SELECT * FROM wp_users"""
     cursor = conn.cursor()
     cursor.execute(select_user)
     result = cursor.fetchall()
 
-    print("Content-type:text/html\n\n")
-    print("<head><title>Employee Database Dump Tool</title></head>\n")
-    print("<body>\n")
-    print("<h1>Employee Database Dump Tool</h1>\n")
+#    print("Content-type:text/html\n\n")
+#    print("<head><title>Employee Database Dump Tool</title></head>\n")
+#    print("<body>\n")
+#    print("<h1>Employee Database Dump Tool</h1>\n")
     print(tabulate(result, tablefmt="html"))
+    print("</body>")
+    print("</html>")
     conn.close()
 
 
