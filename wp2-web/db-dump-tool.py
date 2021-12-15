9cc6b76da24c1eb7c7f67f35db4947879c8b6618
Randy Pope <rpope@Randys-Work-MacBook.local>
2021-12-15 14:48:49 -0500

    added load script and updated readme

---
+++ wp2-web/db-dump-tool.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import mysql.connector
+from tabulate import tabulate
+
+try:
+    print("Content-type:text/html\n\n")
+    print("<head><title>Employee Database Dump Tool</title></head>\n")
+    print("<body>\n")
+    print("<h1>Employee Database Dump Tool</h1>\n")
+
+    conn = mysql.connector.connect(connection_timeout=5, user='wp_user', password='wordpress', host='wp-db',database='wordpress')
+
+except mysql.connector.Error as err:
+#    print("Content-type:text/html\n\n")
+#    print("<head><title>Employee Database Dump Tool</title></head>\n")
+#    print("<body>\n")
+#    print("<h1>Employee Database Dump Tool</h1>\n")
+    print("<h2>Database Connection is broken</h2>")
+    print(err)
+    print("</body>")
+    print("</html>")
+
+else:
+    select_user = """SELECT * FROM wp_users"""
+    cursor = conn.cursor()
+    cursor.execute(select_user)
+    result = cursor.fetchall()
+
+#    print("Content-type:text/html\n\n")
+#    print("<head><title>Employee Database Dump Tool</title></head>\n")
+#    print("<body>\n")
+#    print("<h1>Employee Database Dump Tool</h1>\n")
+    print(tabulate(result, tablefmt="html"))
+    print("</body>")
+    print("</html>")
+    conn.close()
+
+
+
+
+
